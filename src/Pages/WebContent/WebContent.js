b513d8c11ab6801a631ec3add0003760a2a15431
SelnoxInfotech <Selnox93@gmail.com>
2024-05-27 19:09:30 +0530

    ffsdf

--- src/Pages/WebContent/WebContent.js
+++ src/Pages/WebContent/WebContent.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect , useState} from 'react'
 import { SectionCard } from '../../molecules/SectionCard/Index'
 import { SlSocialDropbox } from "react-icons/sl";
 import Box from '@mui/material/Box';
-import { Editor } from "react-draft-wysiwyg";
+import { Editor } from "react-draft-wysiwyg"; 
 import Editortoolbar, { modules, formats } from "./Editortoolbar";
 import { FaTrashAlt } from "react-icons/fa";
 import {
     CitySelect,
     CountrySelect,
     StateSelect,
-} from "react-country-state-city";
-import "react-country-state-city/dist/react-country-state-city.css";
-import ReactQuill from 'react-quill';
+  } from "react-country-state-city";
+  import "react-country-state-city/dist/react-country-state-city.css";
+  import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import draftToHtml from "draftjs-to-html";
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import './Webcontent.css'
+import  './Webcontent.css'
 import { EditorState } from "draft-js";
 import TextField from '@mui/material/TextField';
 import useStyles from '../../Style';
@@ -30,11 +30,11 @@ const WebContent = () => {
     const [contary, setcontary] = React.useState([]);
     const [countryid, setCountryid] = useState(0);
     const [stateid, setstateid] = useState(0);
-    const classes = useStyles()
-    const [isfaq, setIsaq] = useState(false)
+    const classes  = useStyles()
+    const [isfaq , setIsaq] = useState(false)
     const [faqscount, setfaqscount] = React.useState([{
-        title: '',
-        answer: ''
+        title:'',
+        answer:''
     }]);
     const localization = {
         locale: 'en-us',
@@ -56,9 +56,9 @@ const WebContent = () => {
     };
     const [editorState, setEditorState] = React.useState(() =>
         EditorState.createEmpty()
-    );
-
-    const toolbar = {
+      );
+    
+      const toolbar = {
         options: ['blockType', 'inline', 'list', 'colorPicker', 'link', 'image'],
         blockType: {
             inDropdown: true,
@@ -82,7 +82,7 @@ const WebContent = () => {
         }
     };
     const handleChange = (event) => {
-        setAge(event.target.value);
+      setAge(event.target.value);
     };
     // const handleContentStateChange = (contentState) => {
     //     setConvertedContent(draftToHtml(contentState));
@@ -90,15 +90,15 @@ const WebContent = () => {
     // const handleEditorStateChange = (editorState) => {
     //     setEditorState(editorState);
     // };
-    const questionchange = (e, index) => {
-        faqscount[index].title = e.target.value;
+    const questionchange=(e , index)=>{
+        faqscount[index].title =e.target.value;
         setfaqscount([...faqscount])
     }
-    const removefaqbox = (indx) => {
-        let a = faqscount.filter((item, index) => {
+    const removefaqbox=(indx)=>{
+           let a= faqscount.filter((item ,index)=>{
             return index !== indx
-        })
-        setfaqscount(a)
+           })
+           setfaqscount(a)
     }
     console.log(faqscount)
   return (
@@ -115,36 +115,36 @@ const WebContent = () => {
                         <div className='col-lg-3 col-md-6'>
                             <label className='label_faq'>Title </label>
                             <Box >
-                                <FormControl fullWidth>
-                                    <Select
-                                        className={classes.faqselectBox}
-                                        id="demo-simple-select"
-                                        value={age}
-                                        onChange={handleChange}
-                                    >
-                                        <MenuItem value={'Dispensaries'} className={classes.faqtitlelistitem}>Weed Dispensaries</MenuItem>
-                                        <MenuItem value={'Deliveries'} className={classes.faqtitlelistitem}>Weed Deliveries</MenuItem>
-                                    </Select>
-                                </FormControl>
-
+                                   <FormControl fullWidth>
+                                            <Select
+                                                className={classes.faqselectBox}
+                                                id="demo-simple-select"
+                                                value={age}
+                                                onChange={handleChange}
+                                            >
+                                                <MenuItem value={'Dispensaries'} className={classes.faqtitlelistitem}>Weed Dispensaries</MenuItem>
+                                                <MenuItem value={'Deliveries'} className={classes.faqtitlelistitem}>Weed Deliveries</MenuItem>
+                                            </Select>
+                                    </FormControl>
+                               
                             </Box>
                         </div>
                         <div className='col-lg-3 col-md-6'>
-                            <label className='label_faq'> Contary</label>
-
+                        <label className='label_faq'> Contary</label>
+                  
                             <CountrySelect
-                                onChange={(e) => {
-                                    setCountryid(e.id);
-                                    setcontary(e.name)
-                                }}
-                                placeHolder="Select Country"
-                                containerClassName='locationcontainerstyle'
-                                inputClassName='locationinputstyle'
+                            onChange={(e) => {
+                            setCountryid(e.id);
+                            setcontary(e.name)
+                            }}
+                            placeHolder="Select Country"
+                            containerClassName='locationcontainerstyle'
+                            inputClassName='locationinputstyle'
                             />
                         </div>
                         <div className='col-lg-3 col-md-6'>
-                            <label className='label_faq'>State </label>
-                            {/* <Autocomplete
+                        <label className='label_faq'>State </label>
+                        {/* <Autocomplete
                             disablePortal
                             id="combo-box-demo"
                             options={state}
@@ -152,19 +152,19 @@ const WebContent = () => {
                             renderInput={(params) => <TextField {...params} />}
                         /> */}
                             <StateSelect
-                                countryid={countryid}
-                                onChange={(e) => {
-                                    setstateid(e.id);
-                                    setstate(e.name)
-                                }}
-                                containerClassName='locationcontainerstyle'
-                                inputClassName='locationinputstyle'
-                                placeHolder="Select State"
-                            />
+                        countryid={countryid}
+                        onChange={(e) => {
+                        setstateid(e.id);
+                        setstate(e.name)
+                        }}
+                        containerClassName='locationcontainerstyle'
+                        inputClassName='locationinputstyle'
+                        placeHolder="Select State"
+                    />
                         </div>
                         <div className='col-lg-3 col-md-6'>
-                            <label className='label_faq'>City</label>
-                            {/* <Autocomplete
+                        <label className='label_faq'>City</label>
+                        {/* <Autocomplete
                             disablePortal
                             id="combo-box-demo"
                             options={city}
@@ -172,15 +172,15 @@ const WebContent = () => {
                             renderInput={(params) => <TextField {...params} />}
                         /> */}
                             <CitySelect
-                                countryid={countryid}
-                                stateid={stateid}
-                                containerClassName='locationcontainerstyle'
-                                inputClassName='locationinputstyle'
-                                onChange={(e) => {
-                                    setcity(e.name)
-                                }}
-                                placeHolder="Select City"
-                            />
+                        countryid={countryid}
+                        stateid={stateid}
+                        containerClassName='locationcontainerstyle'
+                        inputClassName='locationinputstyle'
+                        onChange={(e) => {
+                        setcity(e.name)
+                        }}
+                        placeHolder="Select City"
+                    />
                         </div>
                     </div>
                     <div className='row my-5'>
@@ -190,27 +190,27 @@ const WebContent = () => {
                                 <TextField id="outlined-basic" placeholder='Title' variant="outlined" className={classes.faqtextfeild} />
                             </div>
                             <div>
-                                <label className='label_faq'>Content</label>
+                            <label className='label_faq'>Content</label>
                                 <Box
-                                    sx={{
-                                        "& .rdw-editor-toolbar": {
-                                            width: "100%",
-                                        },
-                                        "& .rdw-editor-wrapper": {
-                                            height: "240px",
-                                            // width: "991px",
-                                        },
-                                        ".rdw-editor-main": {
-                                            background: "",
-                                            border: "1px solid #c4c4c4",
-                                            padding: "3px",
-                                        },
-                                        "& .quill": {
-                                            height: '250px',
-                                        }
-                                    }}
-                                >
-                                    {/* <Editor
+                                sx={{
+                                "& .rdw-editor-toolbar": {
+                                    width: "100%",
+                                },
+                                "& .rdw-editor-wrapper": {
+                                    height: "240px",
+                                    // width: "991px",
+                                },
+                                ".rdw-editor-main": {
+                                    background: "",
+                                    border: "1px solid #c4c4c4",
+                                    padding: "3px",
+                                },
+                                "& .quill":{
+                                    height:'250px',
+                                }
+                                }}
+                            >
+                                {/* <Editor
                                     editorState={editorState}
                                     toolbar={toolbar}
                                     localization={localization}
@@ -220,11 +220,11 @@ const WebContent = () => {
                                     wrapperClassName="wrapperClassName"
                                     editorClassName="editorClassName"
                                 /> */}
-                                    <Editortoolbar />
-                                    {/* <ReactQuill theme="snow" value={value} onChange={setValue}
+                                <Editortoolbar  />
+                                <ReactQuill theme="snow" value={value} onChange={setValue}
                                     modules={modules}
                                     formats={formats}
-                                /> */}
+                                />
                                 </Box>
                             </div>
                         </div>
@@ -234,37 +234,35 @@ const WebContent = () => {
                             <h3 className=''>FAQs</h3>
                             <div className='faqtoggle'>
                                 <label class="container"> Open
-                                    <input type="checkbox" checked={isfaq} onChange={() => { setIsaq(!isfaq) }} />
-                                    <span class="checkmark"></span>
+                                    <input type="checkbox" checked={isfaq} onChange={()=>{setIsaq(!isfaq)}} />
+                                <span class="checkmark"></span>
                                 </label>
-                            </div>
+                            </div>  
                         </div>
                         {
                             isfaq &&
                             <div className='col-8 mt-5 mx-auto'>
                                 <div className='faqs'>
-
+                                    
                                     <div>
                                         {
-                                            faqscount.map((items, index) => {
+                                            faqscount.map((items , index)=>{
                                                 return <div className='faqinputbox' key={index}>
-                                                    <input type='text' value={items.title} placeholder='Question' onChange={(e) => { questionchange(e, index) }} />
-                                                    <textarea placeholder='Answer' value={items.answer}></textarea>
-                                                    {
-                                                        index !== 0 &&
-                                                        <span className='removefaqbtn' onClick={() => { removefaqbox(index) }}><FaTrashAlt /></span>
-                                                    }
-                                                </div>
+                                                        <input type='text' value={items.title} placeholder='Question' onChange={(e)=>{questionchange(e ,index)}} />
+                                                        <textarea placeholder='Answer' value={items.answer}></textarea>
+                                                            {
+                                                                index !== 0 &&  
+                                                                <span className='removefaqbtn' onClick={()=>{removefaqbox(index)}}><FaTrashAlt /></span>
+                                                            } 
+                                                        </div>
                                             })
                                         }
-
+                                           
                                     </div>
-                                    <button className='addfaqbtn' onClick={() => {
-                                        setfaqscount([...faqscount, {
-                                            title: '',
-                                            answer: ''
-                                        }])
-                                    }}>Add FAQs</button>
+                                    <button className='addfaqbtn' onClick={()=>{setfaqscount([...faqscount ,{
+                                                            title:'',
+                                                            answer:''
+                                                        }])}}>Add FAQs</button>
                                 </div>
                             </div>
                         }
@@ -275,10 +273,10 @@ const WebContent = () => {
                             <button className='faqcancelbtn'>Cancel</button>
                         </div>
                     </div>
-
-                </div>
-            </div>
-        </SectionCard>
-    )
+          
+        </div>
+        </div>
+    </SectionCard> 
+  )
 }
 export default WebContent
